ce8df31e27feb2375aea9d7082adc591ca6ccdc2
Madesh S <madesh200425@gmail.com>
2026-07-24 23:12:16 +0530

    Implement user registration with Django forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
-# Create your views here.
+
+def home(request):
+    return render(request, "home.html")
+
+
+def register(request):
+
+    if request.method == "POST":
+
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect("home")
+
+    else:
+
+        form = RegisterForm()
+
+    return render(request, "accounts/register.html", {
+        "form": form
+    })
